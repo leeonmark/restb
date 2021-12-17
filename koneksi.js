08704b4a5ec2e626fe2ee97378ee08a4233c29c0
ra <lilyzahra9@gmail.com>
2021-12-17 11:02:57 +0700

    change file koneksi

--- koneksi.js
+++ koneksi.js
@@ -6,7 +6,7 @@ const conn = mysql.createConnection({
     host: 'dbkelompoka.gmedia.bz',
     user: 'gmedia_magangb',
     password: ' indo1945!merdeka',
-    database: 'gmedia_magangb',
+    database: 'gmedia_magang',
     connection: 'mysql',
 });
 
