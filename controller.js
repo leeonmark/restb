d04bffecf8d892fcdf5f04cd6b3b8acad4acac13
ra <lilyzahra9@gmail.com>
2021-12-21 08:50:01 +0700

    menampilkan semua data successfull

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("REST API!!", res)
 };
+
+// menampilkan semua data
+exports.tampilsemuadata = function (req, res) {
+    connection.query('SELECT * FROM data', function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
