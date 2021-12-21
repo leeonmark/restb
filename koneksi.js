1779cc8558e3b0af96c8e955ad7dbabd5b61e727
ra <lilyzahra9@gmail.com>
2021-12-21 08:34:45 +0700

    add db to koneksi

--- koneksi.js
+++ koneksi.js
@@ -7,7 +7,7 @@ const conn = mysql.createConnection({
     host: 'dbkelompoka.gmedia.bz',
     user: 'gmedia_magangb',
     password: 'indo1945!merdeka',
-    database: 'gmedia_magang',
+    database: 'gmedia_magangb',
 });
 
 conn.connect((err) => {
